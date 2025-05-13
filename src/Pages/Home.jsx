bd3d79b9fab168ace2429f7cd510db7633d6d3ba
nagarajanvijay69 <nagarajanvijay69@gmail.com>
2025-05-13 18:33:45 +0530

    download

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <span className='two'>Full Stack Developer</span>
       <span className='three'>Passionate about full-stack development especially MERN<br />Currently building chat apps and e-commerce platforms.  <br />Always learning, always coding.</span>
       <span className="nav-btn">
-        <a href="#" className='nav-link nav-1'>My Resume</a>
+        <a href="./resume.pdf" className='nav-link nav-1' download="Nagarajan-Fullstack-Dev.pdf" >My Resume</a>
         <a href="#contact" className='nav-link nav-2'>Contact Me</a>
       </span>
     </div>
